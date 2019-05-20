5f4135d71cf3675c40ac4b55541f9a4ec2532bcc
user.name <jtolosa97@gmail.com>
2019-05-20 09:15:41 +0200

    ya coge la voz

--- src/rosarnl_square.py
+++ src/rosarnl_square.py
@@ -6,42 +6,54 @@ import speech_recognition as sr
 from gtts import gTTS
 import pyglet
 import os 
+import time
+from scipy.io import wavfile
+import pygame
 
-# TODO: Add goals id
+# Add goals id
 goals = ["Goal1", "Goal2","Goal3"]
+r = None
 
-
-# TODO: Create the publisher to publish the topic with the next goal
+# Create the publisher to publish the topic with the next goal
 pub = rospy.Publisher('/rosarnl_node/goalname', String, queue_size=10)
 
 def inicia_micro():
+	global r
 	r = sr.Recognizer()
 	with sr.Microphone() as source:
-    	print("Say something!")
-    	audio = r.listen(source)
-
-def escucha_micro():
-	audio = ''
-	while audio == '':
-		try:
-	    # for testing purposes, we're just using the default API key
-	    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-	    # instead of `r.recognize_google(audio)`
-	    	audio = r.recognize_google(audio)
-		except sr.UnknownValueError:
-	    	speak('I dont understand you ')
-		except sr.RequestError as e:
-	    	print("Could not request results from Google Speech Recognition service; {0}".format(e))
+			print("Say something!")
+			audio = r.listen(source,3)
+	print("Okey")
 	return audio
+
+def escucha_micro(data):
+	global r
+	text = ''
+	r = sr.Recognizer()
+	#fs, data = wavfile.read('pruebasonido.wav')
+	try:
+
+			text = r.recognize_google(data)
+	except sr.UnknownValueError:
+		print("Error 1")
+		speak('I dont understand you')
+		inicia_micro()
+	except sr.RequestError as e:
+		print ("Error 2")
+		speak("Could not request results from Google Speech Recognition service")
+		
+	print('->' + str(text))
+	return text
 def speak(text):
 	tts = gTTS(text)
 	filename = '/tmp/temp.mp3'
 	tts.save(filename)
-
-	music = pyglet.media.load(filename,streaming = False)
+	print('HABLA JO PUTA')
+	music = pyglet.media.load(filename)
 	music.play()
+	pygame.music.Sound(filaname).play()
 
-	sleep(music.duration)
+	time.sleep(music.duration)
 	os.remove(filename)
 
 def state_callback(data):
@@ -49,30 +61,42 @@ def state_callback(data):
 	rospy.loginfo("ARNL path state: " + data.data)
   
 	if data.data == 'REACHED_GOAL':
-		if goals:           #There are more goals
-			pub.publish(goals.pop(0))
-		else:               #Empty goal
-			rospy.loginfo('Final goal reached.')
-			rospy.signal_shutdown('Final goal reached.')
+		speak("I am at the goal")
+
+def understand(text):
+	if 'go to' in text:
+		if 'door' in text:
+			pub.publish('door')
+			print('voy a la puerta')
+		elif 'black board' in text:
+			pub.publish('black_board')
+			print('voy a la pizarra')
+		elif 'circle' in text:
+			pub.publish('circle')
+			print('voy a al centro')
+	elif 'hello' in text:
+		speak('hello')
+		print('ey')
+
 
 
 def main():
 	global goals
 
-	inicia_micro()
-	# Initialise node gain_controller
+	audio = inicia_micro()
 	rospy.init_node('rosarnl_tourgoals_node', anonymous=True)
 
-	rospy.sleep(2) # Seelp for 2 sec
-
-	# TODO: Subscribe to the robot planner state
+	rospy.sleep(1)
+	print("sale sleep")
+	#Subscribe to the robot planner state
 	rospy.Subscriber('rosarnl_node/arnl_path_state', String, state_callback)
 
-	text = escucha_micro()
-	# TODO: Send the robot to the first goal
-	pub.publish(text)
+	text = escucha_micro(audio)
 
-	# Return control to ROS
+	understand(text)
+
+	
+	#Return control to ROS
 	rospy.spin()
 
 if __name__ == '__main__':
