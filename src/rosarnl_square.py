8d49e04613627b79ed1490af5225e234b05e4ed9
user.name <jtolosa97@gmail.com>
2019-05-20 12:51:07 +0200

    casi terminado

--- src/rosarnl_square.py
+++ src/rosarnl_square.py
@@ -9,93 +9,99 @@ import os
 import time
 from scipy.io import wavfile
 import pygame
+from rosarnl.srv import *
 
 # Add goals id
-goals = ["Goal1", "Goal2","Goal3"]
-r = None
-
+finish = False
+door=['door','work']
+circle=['center','circle']
+black_board = ['Blackboard','blackboard','white board']
+finish = False
 # Create the publisher to publish the topic with the next goal
 pub = rospy.Publisher('/rosarnl_node/goalname', String, queue_size=10)
+stop = rospy.Publisher('/rosarnl_node/goalname', String, queue_size=10)
 
-def inicia_micro():
-	global r
+def escucha_micro():
 	r = sr.Recognizer()
+	time.sleep(1)
 	with sr.Microphone() as source:
-			print("Say something!")
-			audio = r.listen(source,3)
+		speak('Say something!')
+		print('Say something!')
+		audio = r.listen(source,5)
 	print("Okey")
-	return audio
-
-def escucha_micro(data):
-	global r
 	text = ''
-	r = sr.Recognizer()
 	#fs, data = wavfile.read('pruebasonido.wav')
 	try:
-
-			text = r.recognize_google(data)
-	except sr.UnknownValueError:
+		
+		text = r.recognize_google(audio)
+	except sr.UnknownValueError :
 		print("Error 1")
 		speak('I dont understand you')
-		inicia_micro()
 	except sr.RequestError as e:
 		print ("Error 2")
 		speak("Could not request results from Google Speech Recognition service")
 		
 	print('->' + str(text))
 	return text
+
 def speak(text):
 	tts = gTTS(text)
 	filename = '/tmp/temp.mp3'
 	tts.save(filename)
-	print('HABLA JO PUTA')
+	#print('HABLA JO PUTA')
 	music = pyglet.media.load(filename)
 	music.play()
-	pygame.music.Sound(filaname).play()
 
 	time.sleep(music.duration)
 	os.remove(filename)
 
 def state_callback(data):
-	global goals
+	global goals, state,finish
 	rospy.loginfo("ARNL path state: " + data.data)
-  
 	if data.data == 'REACHED_GOAL':
 		speak("I am at the goal")
+	
+
 
 def understand(text):
+	global audio,door,circle,black_board, finish
 	if 'go to' in text:
-		if 'door' in text:
+		if  len([elem for elem in door if elem in text]) != 0:
 			pub.publish('door')
 			print('voy a la puerta')
-		elif 'black board' in text:
+		elif len([elem for elem in black_board if elem in text]) != 0:
 			pub.publish('black_board')
 			print('voy a la pizarra')
-		elif 'circle' in text:
+		elif len([elem for elem in circle if elem in text]) != 0:
 			pub.publish('circle')
 			print('voy a al centro')
+
 	elif 'hello' in text:
 		speak('hello')
 		print('ey')
-
+	elif 'finish' in text:
+		print('Voy a terminar porque sois un poco pesaos')
+		finish = True
 
 
 def main():
-	global goals
+	global goals, finish
 
-	audio = inicia_micro()
 	rospy.init_node('rosarnl_tourgoals_node', anonymous=True)
 
 	rospy.sleep(1)
 	print("sale sleep")
 	#Subscribe to the robot planner state
 	rospy.Subscriber('rosarnl_node/arnl_path_state', String, state_callback)
+	while True:
 
-	text = escucha_micro(audio)
+		text = escucha_micro()
+		understand(text)
+		if finish == True:
+			#rospy.ServiceProxy("/rosarnl_node/stop",Stop)
+			rospy.signal_shutdown('Quit')	
 
-	understand(text)
 
-	
 	#Return control to ROS
 	rospy.spin()
 
